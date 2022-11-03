2bc5fdb0e88775e5b77ad959d13a1a156ec3a38d
ehdckf <ehdcjf@funnyfig.com>
2022-11-03 18:50:28 +0900

    ADD: tag schema

---
+++ src/schemas/tag.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { User } from './user.schemas';
+export type TagDocument = Tag & mongoose.Document;
+
+@Schema({ timestamps: true })
+export class Tag {
+
+  @Prop({ required:true })
+  name: string;
+
+  @Prop({ type:mongoose.Schema.Types.ObjectId, ref:User.name,  required: true })
+  owner: mongoose.Types.ObjectId; 
+
+  @Prop()
+  type: number;
+
+  @Prop()
+  color: string;
+
+}
+
+export const TagSchema = SchemaFactory.createForClass(Tag);

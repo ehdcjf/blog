050bffa6c22fa436d9d59504f5ce0c3f87e0ae45
ehdckf <ehdcjf@funnyfig.com>
2022-11-08 18:58:12 +0900

    ADD hbs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, Render} from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,9 @@ export class AppController {
 	constructor(private readonly appService: AppService) { }
 
 	@Get()
-	getHello(): string {
-		return this.appService.getHello();
+	@Render('index')
+	getHello() {
+		console.log('hello')
+		return {message:"Hello world!"};
 	}
 }
